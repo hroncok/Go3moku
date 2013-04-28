dc55ff843b71c5f347ea4df73d03bc8e5892c83c
Miro Hrončok <miro@hroncok.cz>
2013-04-28 10:22:52 +0200

    Javadoc added to ThreadGame

--- src/go3moku/ThreadGame.java
+++ src/go3moku/ThreadGame.java
@@ -5,7 +5,7 @@
 package go3moku;
 
 /**
- *
+ * @brief Helper class to run the Game cycle in a thread
  * @author churchyard
  */
 public class ThreadGame implements Runnable {
@@ -13,16 +13,30 @@ public class ThreadGame implements Runnable {
     private Player x;
     private Player o;
     
+    /**
+     * Prepare the thread.
+     * Give information about Players.
+     * @param x First Player (X)
+     * @param o Second Player (O)
+     */
     public ThreadGame(Player x, Player o) {
         this.x = x;
         this.o = o;
     }
     
+    /**
+     * Start the game.
+     * Uses previously given information about Players
+     */
     @Override
     public void run() {
         Game.startNewGame(x,o);
     }
     
+    /**
+     * Kills the thread.
+     * Game singleton class is still runing, just this cycle has ended.
+     */
     public void kill() {
         Thread.currentThread().interrupt();
     }
